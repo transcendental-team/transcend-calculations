542c9fea442f38ff81927f19fb3697050510f88c
dkundathagard <pinsker@riseup.net>
2016-02-18 22:58:41 -0500

    Add method ln2(), my second implementation of natural logarithm based on the Taylor series. This is not as fast as ln().

--- src/calcfunctions/Functions.java
+++ src/calcfunctions/Functions.java
@@ -3,7 +3,7 @@ package calcfunctions;
 public class Functions {
 	public static final double E = calculateE();
 	
-	/* Calculate the value e using the Newton series:
+	/** Calculate the value e using the Newton series:
 	 * e = ∑(1/(n!))
 	 */
 	public static double calculateE(){
@@ -90,4 +90,46 @@ public class Functions {
 			return 1 / result;
 		}
 	} //end powerOfInt()
+	
+	/* Computes the natural logarithm using the Taylor Series:
+	 * ln(1 + x) = x - (x^2)/2 + (x^3)/3 - (x^4)/4 +- ...
+	 * -1 < x <= 1
+	 * http://www.convict.lu/Jeunes/ultimate_stuff/exp_ln_2.htm
+	 */
+	public static double ln2(double input){
+		//Test for valid input
+		if (input <= 0){
+			return 0.0/0.0; //return NaN
+		}
+		
+		double result = 0;
+		int floor = 0;
+		
+		/* Reduction to valid range of Taylor series: 0 < input < 2.
+		* Floor will be added to the Taylor series result later.
+		*/ 
+		while (input > 2){
+			input /= E;
+			floor++;
+		}
+		
+		/* The Taylor series computes ln of x + 1, 
+		 * so we use (input - 1) for valid ln(input)
+		 */ 
+		double taylorin = input - 1;
+		
+		//ln(1 + x) = x - (x^2)/2 + (x^3)/3 - (x^4)/4 +- ...
+		for (int iter = 1; iter < 1000; iter++){
+			if (iter % 2 == 1) { //if i is odd
+				result += powerOfInt(taylorin, iter)/iter;
+			}
+			else { //if i is even
+				result -= powerOfInt(taylorin, iter)/iter;
+			}
+		}
+		/* return the calculated result + the floor to return
+		 * the natural logarithm of the original input
+		 */
+		return floor + result;
+	}//end ln2()
 }
