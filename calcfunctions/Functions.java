3240821831de2c2df1dd592c787ad9e54ff42835
chao wang <chaowang.ubc@gmail.com>
2016-02-18 20:34:42 -0800

    Update the code naming standard

--- calcfunctions/Functions.java
+++ calcfunctions/Functions.java
@@ -12,41 +12,40 @@ public class Functions {
 	 * @version 2
 	 * @param userInputInRadians as the input variable in radians
 	 */
-	public static double getSinh_TaylorSeries(double user_Input_in_Radians){
+	public static double getSinhTaylor(double aRadians){
 	    double percision = 0.0000000000000000001;
-	    double element_in_Series = user_Input_in_Radians;  //first expansion term is user_Input_in_Radians;
-	    double summation_of_Elements = 0.0;
-	    boolean negative_Input = false;
-	    int expansion_Order = 1;
+	    double elementInSeries = aRadians;  //first expansion term is user_Input_in_Radians;
+	    double summationOfElements = 0.0;
+	    boolean negativeInput = false;
+	    int expansionOrder = 1;
 
 	    // if user input is negative number, we use sinh(x)=-sinh(-x) property. calcualte sinh(-x)
-	    if (user_Input_in_Radians < 0) {
-	    	negative_Input = true;
-	    	user_Input_in_Radians=-user_Input_in_Radians;
-	    	element_in_Series = -element_in_Series;
+	    if (aRadians < 0) {
+	    	negativeInput = true;
+	    	aRadians=-aRadians;
+	    	elementInSeries = -elementInSeries;
 	    }
 	    
  
 	    do {
-	    	summation_of_Elements =summation_of_Elements+ element_in_Series;	 
+	    	summationOfElements =summationOfElements+ elementInSeries;	 
 	    	// second expansion term is x^3/3!, third is x^5/5!, hence need to increase order 2 times
-	    	expansion_Order ++;
-	    	element_in_Series *= user_Input_in_Radians / expansion_Order ;
-	    	expansion_Order ++;
-	    	element_in_Series *= user_Input_in_Radians / expansion_Order ;
+	    	expansionOrder ++;
+	    	elementInSeries *= aRadians / expansionOrder ;
+	    	expansionOrder ++;
+	    	elementInSeries *= aRadians / expansionOrder ;
 	        
 	    	// validate if summation is exceed the limits.
-	    	if (summation_of_Elements > Double.MAX_VALUE) {
+	    	if (summationOfElements > Double.MAX_VALUE) {
 	            System.out.println("Too Large");
 	            break;
 	        }
 	    }
 	    // Quit the summation loop if adding element is less than e-20
-	    while (element_in_Series >= percision);
+	    while (elementInSeries >= percision);
 
 	    //if input is positive, return sinh(x), if input is negative, return -sinh(-x)
-	    return negative_Input ? -summation_of_Elements : summation_of_Elements;
+	    return negativeInput ? -summationOfElements : summationOfElements;
         
 	}
-
 }
