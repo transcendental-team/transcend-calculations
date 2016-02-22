5ae709afdc1076fe4f3e77c03b37d8e9deed2116
dkundathagard <pinsker@riseup.net>
2016-02-22 16:04:17 -0500

    change variable names to show type.

--- Transcendental/src/calculatorfunction/Transfunction.java
+++ Transcendental/src/calculatorfunction/Transfunction.java
@@ -10,14 +10,14 @@ public class Transfunction {
 	 * @return the constant e.
 	 */
 	public static double calculateE(){
-		double result = 1;
+		double dResult = 1;
 		double factorial = 1;
 		//Choose max iterations based on precision
 		for (int i = 1; i < 18; i++){
 			factorial *= i;
-			result += 1/factorial;
+			dResult += 1/factorial;
 		}
-		return result;
+		return dResult;
 	} //End calculateE()
 	
 	/** Computes the natural logarithm using the Taylor Series:
@@ -27,41 +27,41 @@ public class Transfunction {
 	 * @param input is the user input
 	 * @return the natural logarithm of the input
 	 */
-	public static double ln(double input){
+	public static double ln(double dInput){
 		//Test for valid input
-		if (input <= 0){
+		if (dInput <= 0){
 			return 0.0/0.0; //return NaN
 		}
 		
-		double result = 0;
+		double dResult = 0;
 		int floor = 0;
 		
 		/* Reduction to valid range of Taylor series: 0 < input < 2.
 		* Floor will be added to the Taylor series result later.
 		*/ 
-		while (input > 2){
-			input /= E_NUMBER;
+		while (dInput > 2){
+			dInput /= E_NUMBER;
 			floor++;
 		}
 		
 		/* The Taylor series computes ln of x + 1, 
 		 * so we use (input - 1) for valid ln(input)
 		 */ 
-		double taylorin = input - 1;
+		double taylorin = dInput - 1;
 		
 		//ln(1 + x) = x - (x^2)/2 + (x^3)/3 - (x^4)/4 +- ...
 		for (int i = 1; i < 1000; i++){
 			if (i % 2 == 1) { //if i is odd
-				result += powerOfInt(taylorin, i)/i;
+				dResult += powerOfInt(taylorin, i)/i;
 			}
 			else { //if i is even
-				result -= powerOfInt(taylorin, i)/i;
+				dResult -= powerOfInt(taylorin, i)/i;
 			}
 		}
 		/* return the calculated result + the floor to return
 		 * the natural logarithm of the original input
 		 */
-		return floor + result;
+		return floor + dResult;
 	}
 	
 	/** Calculates base to the power of an integer exponent
@@ -70,22 +70,22 @@ public class Transfunction {
 	 * @param int exp is the integer exponent
 	 * @return double result, the result of base^(exp)
 	 */
-	private static double powerOfInt(double base, int exp){
-		double result = 1;
-		if (exp == 0)
+	private static double powerOfInt(double dBase, int iExp){
+		double dResult = 1;
+		if (iExp == 0)
 			return 1;
-		if (exp > 0){
-			for (int i = 0; i < exp; i++){
-				result *= base;
+		if (iExp > 0){
+			for (int i = 0; i < iExp; i++){
+				dResult *= dBase;
 			}
-			return result;
+			return dResult;
 		}
 		//Repeated Code: could be improved
 		else{
-			for (int i = 0; i < (-1 * exp); i++){
-				result *= base;
+			for (int i = 0; i < (-1 * iExp); i++){
+				dResult *= dBase;
 			}
-			return 1 / result;
+			return 1 / dResult;
 		}
 	} //end powerOfInt()
 
