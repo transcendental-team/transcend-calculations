9c25a8d7b715634d19f7ea8acd1cfbf04da13886
Srikanth <s_tadise@live.concordia.ca>
2016-02-22 01:38:30 -0500

    Update Transfunction.java

--- Trancendental/src/calculatorfunction/Transfunction.java
+++ Trancendental/src/calculatorfunction/Transfunction.java
@@ -2,7 +2,7 @@ package calculatorfunction;
 
 public class Transfunction {
 
-	public static final double Pi = pi(); //Pi constant
+	public static final double PI = pi(); //Pi constant
 
 	/**
 	 * @param num negative or positive number.
@@ -35,25 +35,25 @@ public class Transfunction {
 	/**
 	 * Method to Compute the sine of the angle theta using the taylor series expansion.
 	 * sin(x) = x - (x^3/3!) + (x^5/5!) - (x^7/7!) + ...
-	 * @param theta value given in Radians by user.
-	 * @return output the sine of the angle theta which was given in Radians by user.
+	 * @param dRadians value given in Radians by user.
+	 * @return dResult the sine of the angle theta which was given in Radians by user.
 	 * @version 4
 	 */
-	public static final double sine1(double dRadians) {
+	public static final double sine(double dRadians) {
 
-		dRadians %= 2*Pi; //convert dRadians to an angle between -2 PI and 2 PI
-		double output = dRadians; //numerator initially
+		dRadians %= 2*PI; //convert dRadians to an angle between -2 PI and 2 PI
+		double dResult = dRadians; //numerator initially
 		double factorial = 1; //denominator factorial initially.
-		double thetaMultiple = dRadians * dRadians;
+		double radianMultiple = dRadians * dRadians;
 		//iterate to the approximation of positive infinity of type double. 
 		for (int i = 0; factorial < Double.POSITIVE_INFINITY; i++) {
 			factorial *= (2 * i + 2) * (2 * i + 3); //best factorial approximation denominator with each iteration. 
-			dRadians *= thetaMultiple; //numerator power increase by 2
+			dRadians *= radianMultiple; //numerator power increase by 2
 			//Sum of fractional components of the Taylor series expansion.
-			output += (i % 2 == 0 ? -1 : 1) * dRadians / factorial;
+			dResult += (i % 2 == 0 ? -1 : 1) * dRadians / factorial;
 		}
 
-		return output;
+		return dResult;
 
 	}
 
