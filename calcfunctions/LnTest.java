6bf8548c61622d9b919db407fb6e27700a702089
Blynks <eremtek@gmail.com>
2016-02-22 20:46:13 -0500

    Update LnTest.java

--- calcfunctions/LnTest.java
+++ calcfunctions/LnTest.java
@@ -9,7 +9,7 @@ public class LnTest {
 	public void lnTest() {
 			double errorTolerance = 1e-5; 
 			double testNumber0 = Math.PI;
-			double testNumber1 = 0;
+			double testNumber1 = 1;
 
 			assertEquals(Math.log(testNumber0), Functions.ln(testNumber0),
 					errorTolerance);
