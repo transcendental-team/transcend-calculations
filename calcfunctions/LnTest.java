cf48ae65124add81c61c7e1b01d909456147baec
Blynks <eremtek@gmail.com>
2016-02-22 20:44:24 -0500

    Add LnTest

---
+++ calcfunctions/LnTest.java
@@ -0,0 +1,20 @@
+package calculatorfunction;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LnTest {
+	@Test
+	public void lnTest() {
+			double errorTolerance = 1e-5; 
+			double testNumber0 = Math.PI;
+			double testNumber1 = 0;
+
+			assertEquals(Math.log(testNumber0), Functions.ln(testNumber0),
+					errorTolerance);
+			
+			assertEquals(Math.log(testNumber1), Functions.ln(testNumber1),
+					errorTolerance);
+	}
+}
