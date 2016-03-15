4e8fd8877916717a2e982f0c894908c19fea7303
chaowangcanada <chaowang.ubc@gmail.com>
2016-03-15 12:07:21 -0700

    Android Studio 1 Deliverable

---
+++ CalculatorGlossary.java
@@ -0,0 +1,17 @@
+package comchaowangcanada.httpsgithub.calculator;
+
+/**
+ * Created by Chao on 23/02/2016.
+ */
+public class CalculatorGlossary {
+
+     static final String[] TRANSCDENTALGLOSSARY
+             = new String[]{"Symbolic sine function of argument in radian",
+                              "Hyperbolic sine of argument in radians",
+                              "natural logarithm ln(x) of value X",
+                              "the exponential of 10 for value X",
+                              "Square root."};
+
+
+
+}
--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,6 @@
 package comchaowangcanada.httpsgithub.calculator;
 
+import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,16 +12,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 import java.util.Arrays;
 
+import comchaowangcanada.httpsgithub.calculator.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
     private Button[] btnNum = new Button[11];    //0-9 numer operand button and . button
-    private Button[] btnCommand = new Button[9];  //9 operand button
+    private Button[] btnArithmetic = new Button[4];  //4 arithmetic operand button
+    private Button[] btnTranscendental = new Button[5];  //4 arithmetic operand button
     private EditText expressionText = null;
-    private EditText resultText = null;
+    private TextView resultText = null;
     private Button btnClear = null;
     private Button btnDelete = null;
     private Button btnEqual = null;
@@ -28,35 +34,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnRightBracket = null;
     private String lastCommand = null;
     private boolean needClear;
+    boolean isResultString ;
     private double result;
 
     public MainActivity() {
         result = 0.0;
         needClear = false;
+        isResultString = false;
         lastCommand = "=";
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
         //initialize display
         expressionText = (EditText) findViewById(R.id.text1);
-        resultText = (EditText) findViewById(R.id.text2);
+        resultText = (TextView) findViewById(R.id.text2);
 
-        //link operator button
-        btnCommand[0] = (Button) findViewById(R.id.plus_button);
-        btnCommand[1] = (Button) findViewById(R.id.minus_button);
-        btnCommand[2] = (Button) findViewById(R.id.produce_button);
-        btnCommand[3] = (Button) findViewById(R.id.divide_button);
-        btnCommand[4] = (Button) findViewById(R.id.sine_button);
-        btnCommand[5] = (Button) findViewById(R.id.sinh_button);
-        btnCommand[6] = (Button) findViewById(R.id.sqrt_button);
-        btnCommand[7] = (Button) findViewById(R.id.log_button);
-        btnCommand[8] = (Button) findViewById(R.id.power_button);
+        //link arithmetic operator button
+        btnArithmetic[0] = (Button) findViewById(R.id.plus_button);
+        btnArithmetic[1] = (Button) findViewById(R.id.minus_button);
+        btnArithmetic[2] = (Button) findViewById(R.id.produce_button);
+        btnArithmetic[3] = (Button) findViewById(R.id.divide_button);
+
+        //link transcendental operator button
+        btnTranscendental[0] = (Button) findViewById(R.id.sine_button);
+        btnTranscendental[1] = (Button) findViewById(R.id.sinh_button);
+        btnTranscendental[2] = (Button) findViewById(R.id.log_button);
+        btnTranscendental[3] = (Button) findViewById(R.id.power_button);
+        btnTranscendental[4] = (Button) findViewById(R.id.sqrt_button);
 
         //link number button
         btnNum[0] = (Button) findViewById(R.id.zero_button);
@@ -79,8 +96,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnRightBracket = (Button) findViewById(R.id.rightBracket_button);
 
 
-        for (Button bc : btnCommand) {
+        for (Button bc : btnArithmetic) {
             bc.setOnClickListener(this);
+        }
+
+        for (Button bc : btnTranscendental) {
+            bc.setOnClickListener(this);
+
+            bc.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    Button btn= (Button) view;
+                    for (Button bc : btnTranscendental){
+                        if( bc.getText().toString() .equals( btn.getText().toString() ))
+                            clicked(view, Arrays.asList(btnTranscendental).indexOf(bc));
+                    }
+                    return true;
+                }
+            });
         }
 
         for (Button bn : btnNum) {
@@ -94,8 +127,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnClear.setOnClickListener(this);
     }
 
+    public void clicked(View view, int index ){
+        ChromeHelpPopup chromeHelpPopup =new ChromeHelpPopup(MainActivity.this, CalculatorGlossary.TRANSCDENTALGLOSSARY[index]);
+        chromeHelpPopup.show(view);
+    }
+
+
     public void onClick(View view) {
         Button btn = (Button) view;
+
+        // deal with error message clean out
+        if(isResultString){
+            resultText.setTextSize(30);
+            expressionText.setText("");
+            resultText.setText("");
+            isResultString = false;
+        }
+        // setSize(45)= getSize.135. difference is 90. setSize(65) = getSize .195
+        // deal with autosize expression
+        if (expressionText.length() > 10){
+            if (  expressionText.getTextSize() >= 135 )
+                expressionText.setTextSize(45);
+            if (expressionText.length() > 13){
+                expressionText.setTextSize(35);
+                    if (expressionText.length() > 17){
+                            expressionText.setTextSize(25);
+                        }
+                }
+        }
+
+        // deal with has results, want to enter number sign to do new calculation
         String expression = expressionText.getText().toString();
         if (needClear && (
                 btn.getText().equals("0")
@@ -108,38 +169,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 || btn.getText().equals("7")
                 || btn.getText().equals("8")
                 || btn.getText().equals("9")
-                || btn.getText().equals("."))
-                || btn.getText().equals("error")) {
+                || btn.getText().equals("."))) {
             expressionText.setText("");
             resultText.setText("");
-            needClear = false;
-        }
-
-        if (!isEmpty(resultText.getText().toString()) && (
-                btn.getText().equals("+")
-                || btn.getText().equals("-")
-                || btn.getText().equals("×")
-                || btn.getText().equals("÷"))){
-            expressionText.setText(resultText.getText());
-            resultText.setText("");
+            expressionText.setTextSize(55);
             needClear = false;
         }
 
 
-
+        // clean out button
         if (btn.getText().equals("C")) {
             expressionText.setText("");
+            expressionText.setTextSize(55);
             resultText.setText("");
+            needClear = false;
         }
+        // delete button
         else if (btn.getText().equals("del")) {
+            // empty expression has nothing to delete
             if (isEmpty(expression)) {
                 expressionText.setText("");
+                expressionText.setTextSize(55);
                 return;
-            }
+            }// delete the one previous item before cursor.
             else {
-                expressionText.setText(expression.substring(0, expression.length() - 1));
+                int cursorPosition = expressionText.getSelectionStart();
+                if(cursorPosition > 0) {
+                    expressionText.setText(expression.substring(0, cursorPosition - 1)
+                                                + expression.substring(cursorPosition, expression.length()));
+                    expressionText.setSelection(cursorPosition-1);
+                }
             }
-        }
+        }  // preprocess expression and pass expression message to parser class.
         else if (btn.getText().equals("=")) {
             if (isEmpty(expression)) {
                 return;
@@ -148,24 +209,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             expression = expression.replaceAll("÷", "/");
             expression = expression.replaceAll("√", "sqrt");
             resultText.setText(ExpressionParser.eval(expression));
-            needClear = true;   ///needClear = true;
+            if(  resultText.getText().toString().matches(".*[a-z]+.*")){
+                resultText.setTextSize(20);
+                isResultString = true;
+            }
+            needClear = true;
         }
-        else if (!isEmpty(resultText.getText().toString()) &&(
-                btn.getText().equals("sin")
-                        || btn.getText().equals("sinh")
-                        || btn.getText().equals("log")
-                        || btn.getText().equals("10^")
-                        || btn.getText().equals("√"))) {
-            expressionText.setText( btn.getText() + "(" + resultText.getText());
-            expressionText.setSelection(expressionText.getText().length());
-            resultText.setText("");
-            needClear = false;
-        }
+        // any other button than “=" "C", "del"
         else {
-            expressionText.setText(expressionText.getText() + "" + btn.getText());
+            // if result text has value, will add transcendental sign + bring result from result text to expression test
+            if (!isEmpty(resultText.getText().toString()) &&(
+                    btn.getText().equals("sin")
+                            || btn.getText().equals("sinh")
+                            || btn.getText().equals("log")
+                            || btn.getText().equals("10^")
+                            || btn.getText().equals("√"))) {
+                expressionText.setText(btn.getText() + "(" + resultText.getText());
+                expressionText.setSelection(expressionText.getText().length());
+                expressionText.setTextSize(35);
+                resultText.setText("");
+            }
+            // if result text has no value, will add transcendental operator + ( | )
+            else if(isEmpty(resultText.getText().toString()) &&(
+                    btn.getText().equals("sin")
+                            || btn.getText().equals("sinh")
+                            || btn.getText().equals("log")
+                            || btn.getText().equals("10^")
+                            || btn.getText().equals("√"))){
+                int cursorPosition = expressionText.getSelectionStart();
+                expressionText.setText(expressionText.getText().toString().substring(0, cursorPosition) + btn.getText() + "()"
+                        + expressionText.getText().toString().substring(cursorPosition, expressionText.getText().toString().length()));
+                expressionText.setSelection(cursorPosition + btn.getText().length() + 1);
+            }
+            // if result txt has value, and next input is operator,  bring result from result text to expression test, then add operator
+            else if (!isEmpty(resultText.getText().toString()) && (
+                    btn.getText().equals("+")
+                            || btn.getText().equals("-")
+                            || btn.getText().equals("×")
+                            || btn.getText().equals("÷"))){
+                expressionText.setText(resultText.getText() + btn.getText().toString());
+                expressionText.setTextSize(35);
+                expressionText.setSelection(expressionText.getText().length());
+                resultText.setText("");
+                needClear = false;
+            }
+            // if user enter any arithmetic operator or number, cursor moves the new item length
+            else {
+                int cursorPosition = expressionText.getSelectionStart();
+                expressionText.setText(expressionText.getText().toString().substring(0, cursorPosition) + btn.getText()
+                        + expressionText.getText().toString().substring(cursorPosition, expressionText.getText().toString().length()));
+                expressionText.setSelection(cursorPosition + btn.getText().length());
+            }
             needClear = false;
         }
-        expressionText.setSelection(expressionText.getText().length());
     }
 
 
