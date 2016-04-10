21c53a4211ee6151fbaeacd38e568bdd0e6fd001
dkundathagard <dkundathagard@gmail.com>
2016-04-10 15:32:13 -0400

    Create Error.java

---
+++ app/src/main/java/comchaowangcanada/httpsgithub/calculator/Error.java
@@ -0,0 +1,15 @@
+package comchaowangcanada.httpsgithub.calculator;
+/**
+ * Contains the String error messages that can be called from the ExpressionParser class
+ * @author transcendental-team
+ *
+ */
+public class Error {
+	public static String funcCall = "A function must contain an input value enclosed in brackets.";
+	public static String unevenBrackets = "Missing bracket(s)";
+	public static String undefined = "This result is not defined.";
+	public static String outOfRange = "The result is out of range.";
+	public static String missingOp = "Numbers must be separated by an operator";
+	public static String missingVal = "An operator must have a value on either side.";
+			
+}
